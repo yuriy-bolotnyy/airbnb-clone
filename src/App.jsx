621a728bfd03e3f03ad669b2bbe2f6b0d12f6211
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-10-09 23:20:38 -0700

    Adjust styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      {/* <Gallery /> */}
+      <Gallery />
       <Cards />
     </div>
   )
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Card from "./Card"
+
+import "./Cards.css"
+
 import zaferesImg from "../assets/zaferes.png"
 import weddingImg from "../assets/wedding.png"
 import bikingImg from "../assets/biking.png"
 
 const Cards = () => {
     return (
-        <div>
+        <div className="cards">
+            {/* <div style={{background:"red"}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam atque, ipsam quis nostrum beatae veritatis, dolor aperiam sint necessitatibus nemo voluptate debitis provident iste sapiente, mollitia eaque nobis architecto consectetur?</div>
+            <div style={{background:"green"}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nemo quos vero illum tempora officiis, vel consequatur aliquam nam molestiae ratione distinctio architecto, totam cum minima officia. Quae, rerum corporis!</div>
+            <div style={{background:"blue"}}> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, est quisquam molestias nesciunt tenetur architecto nostrum at eius aut dolores suscipit blanditiis temporibus quos ex totam minima maxime cupiditate soluta.</div> */}
              <Card 
                 img={zaferesImg} 
                 rating="5.0" reviews="6" 
